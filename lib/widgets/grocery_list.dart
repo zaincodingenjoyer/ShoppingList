908b1db9e5194accf326193bd995cdb73882009b
zaincodingenjoyer <zainatharpixel@gmail.com>
2025-04-05 11:19:33 +0500

    Shopping List INC

---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => NewItem()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Indent
+      appBar: AppBar(
+        title: const Text('Your Grocieries'),
+        actions: [
+          IconButton(onPressed: _addItem(), icon: const Icon(Icons.add)),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder:
+            (ctx, index) => ListTile(
+              title: Text(groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: groceryItems[index].category.color,
+              ),
+              trailing: Text(groceryItems[index].quantity.toString()),
+            ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //const
+        title: const Text('Add a new item'),
+      ),
+      body: Padding(padding: const EdgeInsets.all(12), child: Text('Da form')),
+    );
+  }
+}
